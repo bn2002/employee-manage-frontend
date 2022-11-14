34652ee01e23f9b0a282381a6261362b7e1368fe
BN2002 <doanhcms@gmail.com>
2022-11-14 16:17:36 +0700

    Init react context

---
+++ src/store/Context.js
@@ -0,0 +1,4 @@
+const { createContext } = require("react");
+
+let Context = createContext();
+export default Context;
---
+++ src/store/action.js
@@ -0,0 +1,11 @@
+const initState = {
+    currentPath: "/",
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+    }
+}
+
+export default reducer;
+export { initState };
